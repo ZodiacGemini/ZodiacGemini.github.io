a340bb3b41c46b876c42b78696177dcbbade8694
Alexander Edvinsson <alexanderedvinsson@Alexanders-MacBook-Pro.local>
2017-05-31 18:00:47 +0200

    removed

--- todolist/src/modules/todoMain/actions/todo/todoActionTypes.js
+++
@@ -1,9 +0,0 @@
-const todoActionTypes = 
-{
-    ADD_TODO: 'ADD_TODO',
-    REMOVE_TODO: 'REMOVE_TODO',
-    TOGGLE_TODO: 'TOGGLE_TODO',
-    REMOVE_DONE_TODOS: 'REMOVE_DONE_TODOS'
-}
-
-export default todoActionTypes;
--- todolist/src/modules/todoMain/constants.js
+++
@@ -1,8 +0,0 @@
-const filters = 
-{ 
-    SHOW_ACTIVE: 'SHOW_ACTIVE', 
-    SHOW_ALL: 'SHOW_ALL', 
-    SHOW_DONE: 'SHOW_DONE'
-}
-
-export default filters;
